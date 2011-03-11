5fcffc5a1cd3dda4f1e506b861a83fbf7e466c93
Ubuntu <ubuntu@ip-10-136-127-160.ap-southeast-1.compute.internal>
2011-03-11 04:04:48 +0000

    Bugfixes to make work with passenger

--- config/rails-passenger.rb
+++ config/rails-passenger.rb
@@ -4,7 +4,7 @@ def run *args
   puts "> #{args.join(' ')}"
   system *args
   if $? != 0
-    raise "Execution of #{args.join(' ')} failed with return code $?"
+    raise "Execution of #{args.join(' ')} failed with return code #{$?}"
   end
 end
 
@@ -15,6 +15,8 @@ else
   run "git checkout #{ENV['commit']}"
 end
 
+ENV['BUNDLE_GEMFILE'] = './Gemfile'
+ENV['RAILS_ENVIRONMENT'] = 'production'
 run "bundle install" if ENV['bundler'] && ENV['bundler'] == 'true'
-run "RAILS_ENVIRONMENT=production rake db:migrate" if ENV['migrate'] && ENV['migrate'] == 'true'
-run "touch tmp/restart.txt" if ENV['restart'] && ENV['restart'] == 'true'
+run "rake db:migrate" if ENV['migrate'] && ENV['migrate'] == 'true'
+run "mkdir -p tmp && touch tmp/restart.txt" if ENV['restart'] && ENV['restart'] == 'true'
