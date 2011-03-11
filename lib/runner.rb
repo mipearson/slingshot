2541b87883f255290fbb2441a7f8b1374e2ee99d
Ubuntu <ubuntu@ip-10-136-127-160.ap-southeast-1.compute.internal>
2011-03-11 05:47:04 +0000

    Add tmpdir, have Runner use with_clean_env before munging its environment

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,13 +1,13 @@
 class Runner
   def self.run_script params={}
-    SlingshotConfig.arguments.merge(params).each do |param, value|
-      if value.nil?
-        ENV.delete(param)
-      else
-        ENV[param] = value.to_s
-      end
-    end
     Bundler.with_clean_env do
+      SlingshotConfig.arguments.merge(params).each do |param, value|
+        if value.nil?
+          ENV.delete(param)
+        else
+          ENV[param] = value.to_s
+        end
+      end
       `bash -c 'cd #{SlingshotConfig.dir} && #{SlingshotConfig.script}' 2>&1`
     end
   end
