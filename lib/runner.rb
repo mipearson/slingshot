87d6e4be1defb65fee12d19a3c5f6c7b26a9dce8
Michael Pearson <mipearson@gmail.com>
2011-03-15 11:52:23 +1100

    Fix runner tests to allow for bundler.with_clean_env

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,14 +1,20 @@
 class Runner
   def self.run_script params={}
     Bundler.with_clean_env do
-      SlingshotConfig.arguments.merge(params).each do |param, value|
-        if value.nil?
-          ENV.delete(param)
-        else
-          ENV[param] = value.to_s
-        end
-      end
+      merge_env params
       `bash -c 'cd #{SlingshotConfig.dir} && #{SlingshotConfig.script}' 2>&1`
     end
   end
+  
+  private
+  
+  def self.merge_env params={}
+    SlingshotConfig.arguments.merge(params).each do |param, value|
+      if value.nil?
+        ENV.delete(param)
+      else
+        ENV[param] = value.to_s
+      end
+    end
+  end    
 end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Runner do
   describe '.run_script' do
     before do
-      SlingshotConfig.stub!(:script => "my_script", :dir => "app_dir", :arguments => {"bob" => "uncle", "mary" => nil})
+      SlingshotConfig.stub!(:script => "my_script", :dir => "app_dir")
+      Runner.stub!(:merge_env)
     end
     
     it "should run the script in the application directory" do
@@ -11,20 +12,31 @@ describe Runner do
       Runner.run_script
     end
     
-    it "should set environment variables based on configuration defaults" do
+    it "should call merge_env with our run arguments" do
       Runner.stub!(:`)
-      Runner.run_script
+      Runner.should_receive(:merge_env).with('bob' => 'uncle')
+      Runner.run_script('bob' => 'uncle')
+    end
+  end
+  
+  describe ".merge_env" do
+    before do
+      SlingshotConfig.stub!(:arguments => {"bob" => "uncle", "mary" => nil})
+    end
+    # We test this private method seperately as Bundler.with_clean_env makes
+    # testing our environment difficult.
+    
+    it "should set environment variables based on configuration defaults" do
+      Runner.send(:merge_env)
       ENV['bob'].should == 'uncle'
       ENV.should_not include 'mary'
     end
     
     it "should override configuration defaults based on passed parameters" do
-      Runner.stub!(:`)
-      Runner.run_script("bob" => 'father', 'mary' => 'mother')
+      Runner.send(:merge_env, {"bob" => 'father', 'mary' => 'mother'})
       ENV['bob'].should == 'father'
       ENV['mary'].should == 'mother'
     end
-    
   end
 end
     
